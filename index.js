76415fdcc030980dc8ff2e98cbc3e96aa3ee2cfa
mincastle98 <alstjd98522@naver.com>
2021-01-23 17:34:11 +0900

    Add middleware concept

--- index.js
+++ index.js
@@ -12,6 +12,11 @@ const handleHome = (req, res) => res.send("Hello from my ass");
 
 const handleProfile = (req, res) => res.send("You are on my profile");
 
+const betweenHome = (req, res, next) => {
+    console.log("I'm between");
+    next();
+}
+app.use(betweenHome);   //use 함수 하단에 존재하는 Route에 대해서만 middleware로서 작동
 
 app.get("/", handleHome);
 

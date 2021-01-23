5828dd60897620cf6af092ba92d96f5ee4fface9
mincastle98 <alstjd98522@naver.com>
2021-01-23 17:49:41 +0900

    Add basic middlewares

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 import express from "express";
+import morgan from "morgan"; //logging을 체크
+import helmet from "helmet";    //기초적인 보안
+import cookieparser from "cookie-parser";   //유저로부터 받은 cookie를 이해하는 방식
+import bodyparser from "body-parser";   //서버가 유저로부터 받은 데이터(form)를 이해하는 방식
 const app = express()
 
 const PORT = 4000;
@@ -9,16 +13,20 @@ const handleListening = () =>
 
 const handleHome = (req, res) => res.send("Hello from my ass");
 
-
 const handleProfile = (req, res) => res.send("You are on my profile");
 
-const betweenHome = (req, res, next) => {
-    console.log("I'm between");
-    next();
-}
-app.use(betweenHome);   //use 함수 하단에 존재하는 Route에 대해서만 middleware로서 작동
+//use 함수 하단에 존재하는 Route에 대해서만 middleware로서 작동
+app.use(cookieparser());    
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({ extended: ture }));  
+app.use(helmet());  
+app.use(morgan("dev")); 
 
-app.get("/", handleHome);
+const middleware = (req, res, next) => {
+    res.send("not happening");
+}
+
+app.get("/", middleware, handleHome);
 
 app.get("/profile", handleProfile);
 

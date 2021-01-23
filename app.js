2b32e58209c6da8f0b4b850e79e196f39882e752
mincastle98 <alstjd98522@naver.com>
2021-01-23 18:28:23 +0900

    Add router concept

--- app.js
+++ app.js
@@ -3,13 +3,8 @@ import morgan from "morgan"; //logging을 체크
 import helmet from "helmet";    //기초적인 보안
 import cookieparser from "cookie-parser";   //유저로부터 받은 cookie를 이해하는 방식
 import bodyparser from "body-parser";   //서버가 유저로부터 받은 데이터(form)를 이해하는 방식
+import { userRouter } from "./router";
 const app = express()
-
-const PORT = 4000;
-
-const handleListening = () =>
-    console.log(`Listening on : http://localhost:${PORT}`);
-
 
 const handleHome = (req, res) => res.send("Hello from my ass");
 
@@ -18,16 +13,14 @@ const handleProfile = (req, res) => res.send("You are on my profile");
 //use 함수 하단에 존재하는 Route에 대해서만 middleware로서 작동
 app.use(cookieparser());    
 app.use(bodyparser.json());
-app.use(bodyparser.urlencoded({ extended: ture }));  
+app.use(bodyparser.urlencoded({ extended: true }));  
 app.use(helmet());  
 app.use(morgan("dev")); 
 
-const middleware = (req, res, next) => {
-    res.send("not happening");
-}
-
-app.get("/", middleware, handleHome);
+app.get("/", handleHome);
 
 app.get("/profile", handleProfile);
 
-app.listen(PORT, handleListening);
+app.use("/user", userRouter);   //누군가 /user 경로에 접속하면 userRouter 전체를 사용
+
+export default app; //내 파일을 import하면 app object를 주겠다는 의미

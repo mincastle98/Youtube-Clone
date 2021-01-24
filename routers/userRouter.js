d0ae3adefa3d0766666dcefb9a96d38494e1168e
mincastle98 <alstjd98522@naver.com>
2021-01-24 18:44:45 +0900

    Fix bug

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -5,8 +5,8 @@ import routes from "../routes";
 const userRouter = express.Router();
 
 userRouter.get("/", users);
-userRouter.get(routes.userDetail, userDetail);
 userRouter.get(routes.editProfile, editProfile);
 userRouter.get(routes.changePassword, changePassword);
+userRouter.get(routes.userDetail, userDetail);  //url에 :를 포함하고 있기 때문에 가장 마지막에 추가해줘야 함
 
 export default userRouter;

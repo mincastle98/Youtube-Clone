f59a594c40bfe89d82914ed069759063d739177c
mincastle98 <alstjd98522@naver.com>
2021-01-26 15:46:40 +0900

    Fix Code

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,13 +1,12 @@
-export const home = (req, res) =>
-    res.render("home", { pageTitle: "Home" });
+import { videos } from "../db"
 
+export const home = (req, res) => {
+    res.render("home", { pageTitle: "Home", videos });
+}
 export const search = (req, res) => {
     const { query: { term: searchingBy } } = req;    //const searchingBy = req.query.term
     res.render("search", { pageTitle: "Search", searchingBy });
 }
-export const videos = (req, res) =>
-    res.render("videos", { pageTitle: "Videos" });
-
 export const upload = (req, res) =>
     res.render("upload", { pageTitle: "Upload" });
 
--- routers/userRouter.js
+++ routers/userRouter.js
@@ -4,7 +4,6 @@ import routes from "../routes";
 
 const userRouter = express.Router();
 
-userRouter.get("/", users);
 userRouter.get(routes.editProfile, editProfile);
 userRouter.get(routes.changePassword, changePassword);
 userRouter.get(routes.userDetail, userDetail);  //url에 :를 포함하고 있기 때문에 가장 마지막에 추가해줘야 함

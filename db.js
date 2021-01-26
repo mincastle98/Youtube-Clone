ea8afb6361447dd942be1c62af93eba704f16583
mincastle98 <alstjd98522@naver.com>
2021-01-26 15:48:03 +0900

    Add base Functions

---
+++ db.js
@@ -0,0 +1,50 @@
+export const videos = [
+    {
+        id: 1111,
+        title: "Video awesome",
+        description: "This is something",
+        views: 24,
+        videoFile: "http://archive.org/details/BigBuckBunny_124",
+        creator: {
+            id: 1212121,
+            name: "Mincastle",
+            email: "asd@asdf.com"
+        }
+    },
+    {
+        id: 2222,
+        title: "Video super",
+        description: "This is something",
+        views: 24,
+        videoFile: "http://archive.org/details/BigBuckBunny_124",
+        creator: {
+            id: 1212121,
+            name: "Mincastle",
+            email: "asd@asdf.com"
+        }
+    },
+    {
+        id: 3333,
+        title: "Video nice",
+        description: "This is something",
+        views: 24,
+        videoFile: "http://archive.org/details/BigBuckBunny_124",
+        creator: {
+            id: 1212121,
+            name: "Mincastle",
+            email: "asd@asdf.com"
+        }
+    },
+    {
+        id: 4444,
+        title: "Video perfect",
+        description: "This is something",
+        views: 24,
+        videoFile: "http://archive.org/details/BigBuckBunny_124",
+        creator: {
+            id: 1212121,
+            name: "Mincastle",
+            email: "asd@asdf.com"
+        }
+    }
+];

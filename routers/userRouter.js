34fe34f463e555ea59d4d07fd6f8e6395354adb2
mincastle98 <alstjd98522@naver.com>
2021-01-26 17:04:46 +0900

    Add Login checking function

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -6,6 +6,6 @@ const userRouter = express.Router();
 
 userRouter.get(routes.editProfile, editProfile);
 userRouter.get(routes.changePassword, changePassword);
-userRouter.get(routes.userDetail, userDetail);  //url에 :를 포함하고 있기 때문에 가장 마지막에 추가해줘야 함
+userRouter.get(routes.userDetail(), userDetail);  //url에 :를 포함하고 있기 때문에 가장 마지막에 추가해줘야 함
 
 export default userRouter;

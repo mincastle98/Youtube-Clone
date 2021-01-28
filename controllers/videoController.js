e6805c648e42ab9b0a98dd8f08dcc59ac1c9deec
mincastle98 <alstjd98522@naver.com>
2021-01-28 20:06:08 +0900

    Complete Home Controller

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,11 +1,18 @@
 import routes from "../routes";
+import Video from "../models/Video";
 
-export const home = (req, res) => {
-  res.render("home", { pageTitle: "Home", videos });
+export const home = async (req, res) => {
+  try {
+    const videos = await Video.find({});
+    res.render("home", { pageTitle: "Home", videos });
+  } catch (err) {
+    console.log(err);
+    res.render("home", { pageTitle: "Home", videos: [] });
+  }
 };
 export const search = (req, res) => {
   const {
-    query: { term: searchingBy },
+    query: { term: searchingBy }
   } = req; //const searchingBy = req.query.term
   res.render("search", { pageTitle: "Search", searchingBy, videos });
 };
@@ -14,7 +21,7 @@ export const getUpload = (req, res) =>
 
 export const postUpload = (req, res) => {
   const {
-    body: { file, title, description },
+    body: { file, title, description }
   } = req;
   // To Do : Upload and save video
   res.redirect(routes.videoDetail(1111));

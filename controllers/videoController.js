a8e027e9b7e9d15ea3079a943a43054575e97e56
mincastle98 <alstjd98522@naver.com>
2021-01-30 18:47:03 +0900

    Add Video detail page

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -6,7 +6,6 @@ export const home = async (req, res) => {
     const videos = await Video.find({});
     res.render("home", { pageTitle: "Home", videos });
   } catch (err) {
-    console.log(err);
     res.render("home", { pageTitle: "Home", videos: [] });
   }
 };
@@ -28,8 +27,17 @@ export const postUpload = async (req, res) => {
   res.redirect(routes.videoDetail(newVideo.id));
 };
 
-export const videoDetail = (req, res) =>
-  res.render("videoDetail", { pageTitle: "Video Detail" });
+export const videoDetail = async (req, res) => {
+  const {
+    params: { id }
+  } = req;
+  try {
+    const video = await Video.findById(id);
+    res.render("videoDetail", { pageTitle: "Video Detail", video });
+  } catch (err) {
+    res.redirect(routes.home);
+  }
+};
 
 export const editVideo = (req, res) =>
   res.render("editVideo", { pageTitle: "Edit Video" });

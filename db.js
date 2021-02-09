c292c9723cd398d6c69c6cb3385cfdda26ef26de
mincastle98 <alstjd98522@naver.com>
2021-02-09 18:40:15 +0900

    Fix code

--- db.js
+++ db.js
@@ -13,7 +13,8 @@ const db = mongoose.connection;
 const handleOpen = () => {
   console.log("✅Connected to DB");
 };
-const handleError = () => console.log1(`❌ Error on DB Connection : ${error}`);
+const handleError = (error) =>
+  console.log(`❌ Error on DB Connection : ${error}`);
 
 db.once("open", handleOpen);
 db.on("error", handleError);

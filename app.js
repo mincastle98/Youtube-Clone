85b28ced40aacddfd2b4253a22446f7f771f3017
mincastle98 <alstjd98522@naver.com>
2021-02-16 23:14:17 +0900

    Add Storing cookies function

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ import helmet from "helmet"; //기초적인 보안
 import cookieparser from "cookie-parser"; //유저로부터 받은 cookie를 이해하는 방식
 import bodyparser from "body-parser"; //서버가 유저로부터 받은 데이터(form)를 이해하는 방식
 import passport from "passport";
+import mongoose from "mongoose";
 import session from "express-session";
+import MongoStore from "connect-mongo";
 import { localsMiddleware } from "./middlewares";
 import routes from "./routes";
 import userRouter from "./routers/userRouter";
@@ -14,6 +16,8 @@ import globalRouter from "./routers/globalRouter";
 import "./passport";
 
 const app = express();
+
+const CookieStore = MongoStore(session);
 
 app.set("view engine", "pug");
 
@@ -28,7 +32,8 @@ app.use(
   session({
     secret: process.env.COOKIE_SECRET,
     resave: true,
-    saveUninitialized: false
+    saveUninitialized: false,
+    store: new CookieStore({ mongooseConnection: mongoose.connection })
   })
 );
 app.use(passport.initialize());

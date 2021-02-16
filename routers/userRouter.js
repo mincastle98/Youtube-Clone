d99c542eafa3838a328e2382696f1d424662a618
mincastle98 <alstjd98522@naver.com>
2021-02-16 23:13:58 +0900

    Add authority about routes

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -2,15 +2,15 @@ import express from "express";
 import {
   changePassword,
   editProfile,
-  userDetail,
-  users,
+  userDetail
 } from "../controllers/userController";
+import { onlyPrivate } from "../middlewares";
 import routes from "../routes";
 
 const userRouter = express.Router();
 
-userRouter.get(routes.editProfile, editProfile);
-userRouter.get(routes.changePassword, changePassword);
+userRouter.get(routes.editProfile, onlyPrivate, editProfile);
+userRouter.get(routes.changePassword, onlyPrivate, changePassword);
 userRouter.get(routes.userDetail(), userDetail); //url에 :를 포함하고 있기 때문에 가장 마지막에 추가해줘야 함
 
 export default userRouter;
